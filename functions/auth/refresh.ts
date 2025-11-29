c7d3fd77320676ca869cf00fedda7fff2cb43722
Abdul Kareem <235873002+abdulk-id@users.noreply.github.com>
2025-11-29 21:31:44 +0500

    Update the functions code

--- functions/auth/refresh.ts
+++ functions/auth/refresh.ts
@@ -1,23 +1,22 @@
 interface Env {
-	SuccessBackend: {
-		fetch: (request: Request) => Promise<Response>;
-	};
+  SuccessBackend: {
+    fetch: (request: Request) => Promise<Response>;
+  };
 }
 
-export default {
-	async fetch(request: Request, env: Env): Promise<Response> {
-    if (request.method === 'OPTIONS') {
-      return new Response(null, {
-        status: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-          'Access-Control-Allow-Credentials': 'true',
-        },
-      });
-    }
 
-		return await env.SuccessBackend.fetch(request);
-	},
+export const onRequest = async (context: { request: Request; env: Env }) => {
+  const { request, env } = context;
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Credentials': 'true',
+      },
+    });
+  }
+  return await env.SuccessBackend.fetch(request);
 };
--- functions/dashboard/integrations/crm.ts
+++ functions/dashboard/integrations/crm.ts
@@ -4,20 +4,18 @@ interface Env {
 	};
 }
 
-export default {
-	async fetch(request: Request, env: Env): Promise<Response> {
-    if (request.method === 'OPTIONS') {
-      return new Response(null, {
-        status: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-          'Access-Control-Allow-Credentials': 'true',
-        },
-      });
-    }
-
-		return await env.SuccessBackend.fetch(request);
-	},
+export const onRequest = async (context: { request: Request; env: Env }) => {
+	const { request, env } = context;
+	if (request.method === "OPTIONS") {
+		return new Response(null, {
+			status: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+				"Access-Control-Allow-Credentials": "true",
+			},
+		});
+	}
+	return await env.SuccessBackend.fetch(request);
 };
